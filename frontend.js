d9e4e1f431bec460369639f0d7c731ad179e1e6e
Kenneth Tschida <158073504+Kenny-Tschida@users.noreply.github.com>
2024-04-30 13:09:55 -0500

    Add files via upload

--- frontend.js
+++ frontend.js
@@ -5,6 +5,17 @@ function App() {
   const [view, setView] = useState("Home");
   const [park, setPark] = useState([]);
   const [hike, setHike] = useState([]);
+  const [singleHike, setSingleHike] = useState({
+    id: 0,
+    title: "",
+    distance: "",
+    elevation: "",
+    difficulty: "",
+    image: "",
+    duration: "",
+    location: "",
+    rating: 0,
+  });
   const [planner, setPlanner] = useState([]);
   const {
     register,
@@ -42,81 +53,86 @@ function App() {
     // Set the view based on the park name
     switch (parkName) {
       case "Grand Tetons National Park":
-      fetch("http://localhost:8081/gethikes/1")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-      break;
+        fetch("http://localhost:8081/gethikes/1")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+        break;
       case "Zion National Park":
         fetch("http://localhost:8081/gethikes/2")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
         break;
       case "Yosemite National Park":
         fetch("http://localhost:8081/gethikes/3")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
         break;
       case "Yellowstone National Park":
         fetch("http://localhost:8081/gethikes/4")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
         break;
       case "Badlands National Park":
         fetch("http://localhost:8081/gethikes/5")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
         break;
       case "Joshua Tree National Park":
         fetch("http://localhost:8081/gethikes/6")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Fetched data:", data);
-          setHike(data.hikes);
-          setView(parkName);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Fetched data:", data);
+            setHike(data.hikes);
+            setView(parkName);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
         break;
       default:
         // Handle default case if needed
         break;
     }
+  }
+
+  function viewHike(data) {
+    setSingleHike(data);
+    setView("Hike");
   }
 
   function viewPlanner() {
@@ -162,27 +178,32 @@ function App() {
       </div>
     ));
 
-    const viewAllHikes = hike.map((hike) => (
-      <div className="col" key={hike.id}>
-        <div id="lower" className="card shadow-sm">
-          <img
-            src={hike.image}
-            className="card-img-top"
-            width="500"
-            height="400"
-            alt="Hike"
-          />
-          <div className="card-body">
-            <h5 className="card-text"><strong>{hike.title}</strong></h5>
-            <p className="card-text">Distance: {hike.distance}</p>
-            <p className="card-text">Elevation: {hike.elevation}</p>
-            <p className="card-text">Duration: {hike.duration}</p>
-            <p className="card-text">Difficulty: {hike.difficulty}</p>
-            <p className="card-text">Rating: {hike.rating}</p>
-          </div>
+  const viewAllHikes = hike.map((el) => (
+    <div className="col" key={el.id}>
+      <div id="lower" className="card shadow-sm">
+        <img
+          src={el.image}
+          className="card-img-top"
+          width="500"
+          height="400"
+          alt="Hike"
+        />
+        <div className="card-body">
+          <h5 className="card-text">
+            <strong>{el.title}</strong>
+          </h5>
+          <p className="card-text">Distance: {el.distance}</p>
+          <p className="card-text">Elevation: {el.elevation}</p>
+          <p className="card-text">Duration: {el.duration}</p>
+          <p className="card-text">Difficulty: {el.difficulty}</p>
+          <p className="card-text">Rating: {el.rating}</p>
+          <button className="btn btn-primary" onClick={() => viewHike(el)}>
+            View {el.title}
+          </button>
         </div>
       </div>
-    ));    
+    </div>
+  ));
 
   return (
     <div>
@@ -235,28 +256,37 @@ function App() {
                   viewing the details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
           </div>
         )}{" "}
-        {view === "Zion National Park" && (<div>
+        {view === "Zion National Park" && (
+          <div>
             <div>
               <div id="con" class="container">
                 <p id="trail">
                   Below you will find all of the hikes we currently offer for
-                  Zion National Park. All you need to do is click on the
-                  button of a hike that interests you, and then you'll be
-                  viewing the details of each hike!
+                  Zion National Park. All you need to do is click on the button
+                  of a hike that interests you, and then you'll be viewing the
+                  details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
-          </div>)}{" "}
-        {view === "Yosemite National Park" && (<div>
+          </div>
+        )}{" "}
+        {view === "Yosemite National Park" && (
+          <div>
             <div>
               <div id="con" class="container">
                 <p id="trail">
@@ -266,12 +296,17 @@ function App() {
                   viewing the details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
-          </div>)}{" "}
-        {view === "Yellowstone National Park" && (<div>
+          </div>
+        )}{" "}
+        {view === "Yellowstone National Park" && (
+          <div>
             <div>
               <div id="con" class="container">
                 <p id="trail">
@@ -281,12 +316,17 @@ function App() {
                   viewing the details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
-          </div>)}{" "}
-        {view === "Badlands National Park" && (<div>
+          </div>
+        )}{" "}
+        {view === "Badlands National Park" && (
+          <div>
             <div>
               <div id="con" class="container">
                 <p id="trail">
@@ -296,12 +336,17 @@ function App() {
                   viewing the details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
-          </div>)}{" "}
-        {view === "Joshua Tree National Park" && (<div>
+          </div>
+        )}{" "}
+        {view === "Joshua Tree National Park" && (
+          <div>
             <div>
               <div id="con" class="container">
                 <p id="trail">
@@ -311,36 +356,85 @@ function App() {
                   viewing the details of each hike!
                 </p>
               </div>
-              <div id="column" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div
+                id="column"
+                class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+              >
                 {viewAllHikes}
               </div>
             </div>
-          </div>)}{" "}
+          </div>
+        )}{" "}
+        {view === "Hike" && (
+          <div>
+            <div>
+              <div id="divPic">
+                <img
+                  src={singleHike.image}
+                  className="card-img-top"
+                  width="500"
+                  height="400"
+                  alt="Hike"
+                />
+                <gmp-map
+                  center={singleHike.location}
+                  zoom="11"
+                  map-id="DEMO_MAP_ID"
+                >
+                  <gmp-advanced-marker
+                    position={singleHike.location}
+                    title="Lakeshore Trail"
+                  ></gmp-advanced-marker>
+                </gmp-map>
+              </div>
+            </div>
+            <div id="divTitle">
+              <h1>
+                <strong>{singleHike.title}</strong>
+              </h1>
+              <div id="divDescription">
+              <p className="card-text">Distance: {singleHike.distance}</p>
+          <p className="card-text">Elevation: {singleHike.elevation}</p>
+          <p className="card-text">Duration: {singleHike.duration}</p>
+          <p className="card-text">Difficulty: {singleHike.difficulty}</p>
+          <p className="card-text">Rating: {singleHike.rating}</p>
+              </div>
+            </div>
+          </div>
+        )}{" "}
         {view === "Planner" && (
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
-          <div className="form-group">
-            <input
-              {...register("fullName", { required: true })}
-              placeholder="Full Name"
-              className="form-control"
-            />
-            {errors.fullName && (
-              <p className="text-danger">Full Name is required.</p>
-            )}
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+              <div className="form-group">
+                <input
+                  {...register("fullName", { required: true })}
+                  placeholder="Full Name"
+                  className="form-control"
+                />
+                {errors.fullName && (
+                  <p className="text-danger">Full Name is required.</p>
+                )}
+              </div>
+              <div className="form-group">
+                <input
+                  {...register("email", {
+                    required: true,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                  placeholder="Email"
+                  className="form-control"
+                />
+                {errors.email && (
+                  <p className="text-danger">Email is required.</p>
+                )}
+              </div>
+              <button class="btn btn-primary" type="submit">
+                Submit
+              </button>
+            </form>
           </div>
-          <div className="form-group">
-            <input
-              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-              placeholder="Email"
-              className="form-control"
-            />
-            {errors.email && <p className="text-danger">Email is required.</p>}
-          </div>
-          <button class="btn btn-primary" type="submit">Submit</button>
-        </form>
-        </div>)}{" "}
-        {view === "Confirm" && (<div></div>)}{" "}
+        )}{" "}
+        {view === "Confirm" && <div></div>}{" "}
         {view === "Info" && (
           <body>
             <div id="lower" class="row">
